7474c78ff245dc18ec523bd74f28ad9a0f9c90d2
sourasen1011 <souradeepsen2711@gmail.com>
2023-08-05 22:19:48 +0100

    iterate over models to gen distributions

--- time_invariant_survival/main.py
+++ time_invariant_survival/main.py
@@ -1,9 +1,33 @@
-from time_invariant_surv import *
+from time_invariant_surv import Time_Invariant_Survival
+from traditional_models import CPH, AFT, RSF
+from general_utils import *
+from model_utils import *
+from losses import *
+from models import *
 
-def run_tis():
+def run_fitters():
     '''
     helper function to run time invariant survival
     '''
+    # have a dictionary to store all the metrics
+    eval_dict = {
+        'tis':{
+            'cindex':0 , 
+            'ibs':0
+            },
+        'cph':{
+            'cindex':0 , 
+            'ibs':0
+            },
+        'aft':{
+            'cindex':0 , 
+            'ibs':0
+            },
+        'rsf':{
+            'cindex':0 , 
+            'ibs':0
+            },
+        }
     # Get configs
     with open(config_file_path, "r") as file:
         configs = json.load(file)
@@ -19,8 +43,8 @@ def run_tis():
     # Read the pickled DataFrame
     with open('../05_preprocessing_emr_data/data/consolidated_pat_tbl.pickle', 'rb') as file:
         consolidated_pat_tbl = pickle.load(file)
-
-    # instantiate
+    #-----------------------------------------------------------------------------------------------
+    # instantiate - Time Invariant Survival
     tis = Time_Invariant_Survival(
         configs = configs, 
         train_data = x_train,
@@ -28,13 +52,56 @@ def run_tis():
         val_data = x_val
     )
 
+    # fit
     tis.fit(verbose = True)
-    mean_ , up_ , low_ , y_test_dur , y_test_event = tis.predict()
-    # obj.visualize(mean_ , up_ , low_ , _from = 40 , _to = 50 )
-    cindex , ibs = tis.evaluation(mean_ , y_test_dur , y_test_event, plot = False)
+    mean_ , up_ , low_ , y_test_dur , y_test_event = tis.predict() # Visualize -> tis.visualize(mean_ , up_ , low_ , _from = 40 , _to = 50 )
+    tis_cindex , tis_ibs = tis.evaluation(mean_ , y_test_dur , y_test_event, plot = False)
+    
+    # populate corresponding values in eval dict
+    eval_dict['tis']['cindex'] = tis_cindex
+    eval_dict['tis']['ibs'] = tis_ibs
+    
+    #-----------------------------------------------------------------------------------------------
+    # instantiate - CPH
+    cph = CPH(configs = configs, train_data = x_train, test_data = x_test, val_data = x_val)
 
-    return cindex , ibs
+    # fit
+    cph.fit()
+    # eval
+    cph_cindex , cph_ibs = cph.eval(fitter_is_rsf = False)
+        
+    # populate corresponding values in eval dict
+    eval_dict['cph']['cindex'] = cph_cindex
+    eval_dict['cph']['ibs'] = cph_ibs
+
+    #-----------------------------------------------------------------------------------------------
+    # instantiate - AFT
+    aft = AFT(configs = configs, train_data = x_train, test_data = x_test, val_data = x_val)
+
+    # fit
+    aft.fit()
+    # eval
+    aft_cindex , aft_ibs = aft.eval(fitter_is_rsf = False)
+        
+    # populate corresponding values in eval dict
+    eval_dict['aft']['cindex'] = aft_cindex
+    eval_dict['aft']['ibs'] = aft_ibs
+
+    #-----------------------------------------------------------------------------------------------
+    # instantiate - RSF
+    rsf = RSF(configs = configs, train_data = x_train, test_data = x_test, val_data = x_val)
+
+    # fit
+    rsf.fit()
+    # eval
+    rsf_cindex , rsf_ibs = rsf.eval(fitter_is_rsf = True)
+        
+    # populate corresponding values in eval dict
+    eval_dict['rsf']['cindex'] = rsf_cindex
+    eval_dict['rsf']['ibs'] = rsf_ibs
+
+    return eval_dict    
 
 if __name__ == "__main__":
-    cindex , ibs = run_tis()
-    print(cindex , ibs)
+    eval_dict = run_fitters()
+    print(eval_dict)
---
+++ time_invariant_survival/traditional_models.py
@@ -0,0 +1,94 @@
+from general_utils import *
+
+# Traditional
+from lifelines import CoxPHFitter
+from lifelines import WeibullAFTFitter
+
+# Tree-Based
+from sksurv.ensemble import RandomSurvivalForest
+
+class _traditional_fitter:
+    '''
+    simple wrapper class for cox proportional hazards
+    '''
+    def __init__(self , configs , train_data , test_data , val_data):
+        self.train_data = train_data
+        self.test_data = test_data
+        self.val_data = val_data
+        self.configs = configs
+        # state var
+        self.fitted = False
+        self.fitter = None
+
+    def eval(self , fitter_is_rsf = False):
+        if not self.fitted:
+            raise Exception('Model not fitted yet!')
+            
+        # get evaluation - with a tweak for RSF. not the best. but works for now.
+        if not fitter_is_rsf:
+            # predict
+            _surv = self.fitter.predict_survival_function(self.test_data.iloc[: , :-2])
+            ev = EvalSurv(pd.DataFrame(_surv), self.test_data['time_to_event'].to_numpy(), self.test_data['death'].to_numpy(), censor_surv='km')
+        else:
+            # predict
+            _surv = self.fitter.predict_survival_function(self.test_data.iloc[: , :-2] , return_array = True)
+            ev = EvalSurv(pd.DataFrame(_surv.T), self.test_data['time_to_event'].to_numpy(), self.test_data['death'].to_numpy(), censor_surv='km')
+        
+        # get time grid
+        time_grid_div = self.configs['time_invariant']['eval']['time_grid_div']
+        time_grid = np.linspace(self.test_data['time_to_event'].min(), self.test_data['time_to_event'].max(), time_grid_div)
+        # get metrics
+        cindex = ev.concordance_td('antolini')
+        ibs = ev.integrated_brier_score(time_grid)
+        return cindex , ibs
+    
+class CPH(_traditional_fitter):
+    def __init__(self , configs , train_data , test_data , val_data):
+        super(CPH , self).__init__(configs , train_data , test_data , val_data)
+
+    def fit(self):
+        # init CPH
+        cph = CoxPHFitter(penalizer = 0.1)
+        # fit
+        cph.fit(self.train_data, duration_col='time_to_event', event_col='death', fit_options = {'step_size':0.1})
+        # assign the fitted model to a class attr
+        self.fitter = cph
+        # change state var
+        self.fitted = True
+
+class AFT(_traditional_fitter):
+    def __init__(self , configs , train_data , test_data , val_data):
+        super(AFT , self).__init__(configs , train_data , test_data , val_data)
+
+    def fit(self):
+        # init AFT
+        aft = WeibullAFTFitter(penalizer = 0.01)
+        eps = 1e-8
+        self.train_data['time_to_event'] = self.train_data['time_to_event'] + eps
+        # fit
+        aft.fit(self.train_data, duration_col='time_to_event', event_col='death')
+
+        # assign the fitted model to a class attr
+        self.fitter = aft
+        # change state var
+        self.fitted = True   
+
+class RSF(_traditional_fitter):
+    def __init__(self , configs , train_data , test_data , val_data):
+        super(RSF , self).__init__(configs , train_data , test_data , val_data)
+
+    def fit(self):
+        # Train - Create a structured array
+        y_train = np.array([(x, y) for x, y in zip(self.train_data['death'].astype('bool') , self.train_data['time_to_event'])],
+                                    dtype=[('death', bool) , ('time_to_event', int)])
+
+        # init RSF
+        rsf = RandomSurvivalForest(
+            n_estimators=100, min_samples_split=10, min_samples_leaf=15, n_jobs=-1, oob_score = True
+        )
+        rsf.fit(self.train_data.iloc[: , :-2], y_train)
+
+        # assign the fitted model to a class attr
+        self.fitter = rsf
+        # change state var
+        self.fitted = True   
